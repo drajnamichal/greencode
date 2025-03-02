d70d261ca1221d2c42673f2e49c9e84034c9ef5d
Michal Drajna <michal.drajna@investown.cz>
2025-03-02 21:11:48 +0100

    👌 IMPROVE: Update expiration and localStorage values in standard_user.json; simplify authentication setup in tests

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,13 +1,8 @@
-import { test as setup, expect, chromium } from '@playwright/test';
+import { test as setup, expect } from '@playwright/test';
 
 const authFile = '.auth/standard_user.json';
 
-setup('Authentication', async () => {
-  try {
-    const browser = await chromium.launch({ headless: true });
-    const context = await browser.newContext();
-
-    const page = await context.newPage();
+setup('Authentication', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     await page.locator('[data-test="username"]').fill('standard_user');
     await page.locator('[data-test="password"]').fill('secret_sauce');
@@ -16,10 +11,4 @@ setup('Authentication', async () => {
 
     // Save storage state
     await page.context().storageState({ path: authFile });
-
-    // Close the browser
-    await browser.close();
-  } catch (error) {
-    console.error('Authentication failed:', error);
-  }
 });
