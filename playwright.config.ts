c922d885135277a51c0f1657c78b4b240eac9193
Michal Drajna <michal.drajna@investown.cz>
2025-03-07 06:08:17 +0100

    👌 IMPROVE: Update standard_user.json expiration and localStorage values; add screenshot directory to Playwright config and specify screenshot name in visual test

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,6 +18,7 @@ import { Config } from "./config";
  */
 export default defineConfig({
   testDir: './tests',
+  snapshotDir: "./screenshots",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -2,5 +2,5 @@ import test, { expect } from '../fixtures/basePages';
 
 test('Visual comparison test on Saucedemo', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/inventory.html');
-  await expect(page).toHaveScreenshot();
+  await expect(page).toHaveScreenshot('homepage.png');
 });
